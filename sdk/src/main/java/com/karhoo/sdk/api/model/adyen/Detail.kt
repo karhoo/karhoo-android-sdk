281f9a8c913b96ff286c331c54ca9e687553e3b0
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-08-12 09:12:08 +0100

    Add config map to response

--- sdk/src/main/java/com/karhoo/sdk/api/model/adyen/Detail.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/adyen/Detail.kt
@@ -8,5 +8,6 @@ import kotlinx.android.parcel.Parcelize
 data class Detail(@SerializedName("items") val items: List<Item>,
                   @SerializedName("key") val key: String,
                   @SerializedName("optional") val optional: Boolean,
-                  @SerializedName("type") val type: String)
+                  @SerializedName("type") val type: String,
+                  val config: Map<String,String>)
     : Parcelable
