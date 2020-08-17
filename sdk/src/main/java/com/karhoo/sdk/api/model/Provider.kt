5a9a459007b276656c2d8e55aa38ae1a20067c83
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-08-17 14:39:23 +0100

    Create Provider

--- sdk/src/main/java/com/karhoo/sdk/api/model/Provider.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/Provider.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class PaymentProvider(@SerializedName("id") val id: String = "",
-                           @SerializedName("loyalty_programmes") val loyalty:
+data class Provider(@SerializedName("id") val id: String = "",
+                    @SerializedName("loyalty_programmes") val loyalty:
                            List<LoyaltyProgramme>? = listOf())
     : Parcelable
