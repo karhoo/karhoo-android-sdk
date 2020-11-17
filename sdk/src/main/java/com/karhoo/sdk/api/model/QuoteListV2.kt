c89d861b2319f919aabad25877132ca360ad832c
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-11-17 14:55:34 +0000

    Delete redundant classes

--- sdk/src/main/java/com/karhoo/sdk/api/model/QuoteListV2.kt
+++
@@ -1,8 +0,0 @@
-package com.karhoo.sdk.api.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class QuoteListV2(val categories: Map<String, List<QuoteV2>>,
-                       val id: QuoteId) : Parcelable
