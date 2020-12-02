13370264a6052301d430c823d6bc1dc6e789dc02
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2020-12-02 10:15:49 +0000

    [MOB-3055] Fix POI usage (#76)
    * Fix for POI and aligned names with iOS
    
    * no message

--- sdk/src/main/java/com/karhoo/sdk/api/model/LocationInfo.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/LocationInfo.kt
@@ -9,7 +9,7 @@ data class LocationInfo(
         @SerializedName(value = "position", alternate = ["latlong"]) val position: Position? = null,
         @SerializedName("place_id") val placeId: String = "",
         @SerializedName("time_zone") val timezone: String = "",
-        @SerializedName("poi_type") val poiType: Poi = Poi.NOT_SET,
+        @SerializedName("poi_type") val poiType: PoiType = PoiType.NOT_SET,
         @SerializedName("details") val details: PoiDetails = PoiDetails(),
         @SerializedName("meeting_point") val meetingPoint: MeetingPoint = MeetingPoint(),
         @SerializedName("address") private val address: Address = Address())
