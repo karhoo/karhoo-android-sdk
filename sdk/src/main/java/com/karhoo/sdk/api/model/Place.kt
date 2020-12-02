9abd0af94139157ffa598be9352ca0ec547640fd
EdwardW2 <edward@wilkins-family.org.uk>
2020-12-02 10:40:04 +0000

    Building

--- sdk/src/main/java/com/karhoo/sdk/api/model/Place.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/Place.kt
@@ -7,4 +7,4 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class Place(@SerializedName("place_id") val placeId: String = "",
                  @SerializedName("display_address") val displayAddress: String = "",
-                 @SerializedName("type") val type: PoiType? = null) : Parcelable
+                 @SerializedName("type") val type: PoiDetailsType? = null) : Parcelable
