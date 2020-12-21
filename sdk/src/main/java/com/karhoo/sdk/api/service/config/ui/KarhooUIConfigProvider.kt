84074b6b3a7c2483afc104c904343e0c8b009f9a
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-12-21 11:33:27 +0000

    Use secure.properties and add new org ids

--- sdk/src/main/java/com/karhoo/sdk/api/service/config/ui/KarhooUIConfigProvider.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/service/config/ui/KarhooUIConfigProvider.kt
@@ -1,5 +1,6 @@
 package com.karhoo.sdk.api.service.config.ui
 
+import com.karhoo.sdk.BuildConfig
 import com.karhoo.sdk.api.KarhooError
 import com.karhoo.sdk.api.model.Organisation
 import com.karhoo.sdk.api.model.UIConfig
@@ -14,31 +15,39 @@ class KarhooUIConfigProvider : UIConfigProvider {
 
     override fun fetchConfig(uiConfigRequest: UIConfigRequest, organistionId: Organisation): Deferred<Resource<UIConfig>> {
 
-        //defaultForKarhooUsers : a1013897-132a-456c-9be2-636979095ad9
-        //portalUser: 089a666b-a6ce-4e75-8d7f-12d8f0208f1b
-        //Mon Chauffer: 5a54722d-e699-4da6-801f-a5652e6e31f7
-
         // TODO: Review before making publishing this project to open source
 
         val uiSettings = UISettings(
                 mapOf(
-                        "ed5ae432-1ff7-4d69-a4a6-2d4a65c81b0c" to UISetting(
+                        BuildConfig.KARHOO_TRAVELLER_ORGANISATION_ID to UISetting(
                                 mapOf(
-                                        "additionalFeedbackView" to UIConfig(false),
+                                        "additionalFeedbackView" to UIConfig(true),
                                         "addressMapOptions" to UIConfig(false))),
-                        "a1013897-132a-456c-9be2-6979095ad9" to UISetting(
+                        BuildConfig.KARHOO_TRAVELLER_ORGANISATION_ID to UISetting(
                                 mapOf(
-                                        "additionalFeedbackView" to UIConfig(false),
+                                        "additionalFeedbackView" to UIConfig(true),
                                         "addressMapOptions" to UIConfig(false))),
-                        "5fc4f33b-2832-466e-9943-8728589ef727" to UISetting(
+                        BuildConfig.KARHOO_PROD_BRAINTREE_DEFAULT_ORGANISATION_ID to UISetting(
                                 mapOf(
-                                        "additionalFeedbackView" to UIConfig(false),
+                                        "additionalFeedbackView" to UIConfig(true),
                                         "addressMapOptions" to UIConfig(false))),
-                        "23661866-6554-46bf-977e-21430a3e1f22" to UISetting(
+                        BuildConfig.KARHOO_SANDBOX_BRAINTREE_DEFAULT_ORGANISATION_ID to UISetting(
                                 mapOf(
-                                        "additionalFeedbackView" to UIConfig(false),
+                                        "additionalFeedbackView" to UIConfig(true),
                                         "addressMapOptions" to UIConfig(false))),
-                        "5a54722d-e699-4da6-801f-a5652e6e31f7" to UISetting(
+                        BuildConfig.KARHOO_SANDBOX_ADYEN_DEFAULT_ORGANISATION_ID to UISetting(
+                                mapOf(
+                                        "additionalFeedbackView" to UIConfig(true),
+                                        "addressMapOptions" to UIConfig(false))),
+                        BuildConfig.KARHOO_STAGING_BRAINTREE_DEFAULT_ORGANISATION_ID to UISetting(
+                                mapOf(
+                                        "additionalFeedbackView" to UIConfig(true),
+                                        "addressMapOptions" to UIConfig(false))),
+                        BuildConfig.KARHOO_STAGING_ADYEN_DEFAULT_ORGANISATION_ID to UISetting(
+                                mapOf(
+                                        "additionalFeedbackView" to UIConfig(true),
+                                        "addressMapOptions" to UIConfig(false))),
+                        BuildConfig.KARHOO_US_ORGANISATION_ID to UISetting(
                                 mapOf(
                                         "additionalFeedbackView" to UIConfig(true),
                                         "addressMapOptions" to UIConfig(false)))))
