cd07c0ec18d007c1ddc639df40c14075bd4d54bb
Mostafa Hadian <mostafa.hadian@karhoo.com>
2021-01-13 17:11:00 +0000

    Remove hardcoded value for the quoteStatus (#89)
    * Remove hardcoded value for the quoteStatus
    
    * add secure.properties to .gitignore file
    
    * Bump canary version number
    
    * Bump canary version number
    
    * Bump canary version number

--- sdk/src/main/java/com/karhoo/sdk/api/model/QuoteStatus.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/QuoteStatus.kt
@@ -2,10 +2,11 @@ package com.karhoo.sdk.api.model
 
 import com.google.gson.annotations.SerializedName
 
-enum class QuoteStatus constructor(private val status: String) {
+enum class QuoteStatus constructor(val value: String) {
 
     @SerializedName("PROGRESSING")
     PROGRESSING("PROGRESSING"),
+
     @SerializedName("COMPLETED")
     COMPLETED("COMPLETED"),
 
@@ -13,7 +14,7 @@ enum class QuoteStatus constructor(private val status: String) {
 
     override fun toString(): String {
         val sb = StringBuilder("QuoteStatus{")
-        sb.append("status='").append(status).append('\'')
+        sb.append("status='").append(value).append('\'')
         sb.append('}')
         return sb.toString()
     }
