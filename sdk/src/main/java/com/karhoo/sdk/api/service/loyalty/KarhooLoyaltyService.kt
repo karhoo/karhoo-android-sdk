ed63baf79c6bfb6493944b6b08cfaac1aebda7c7
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2021-11-30 08:24:00 +0200

    Mob-3640 add loyalty program APIs (#131)
    * first attempt
    
    * Update KarhooLoyaltyService.kt
    
    * newLine?
    
    * newLines
    
    * Update APITemplate.kt
    
    * Update APITemplate.kt
    
    * a
    
    * Update APITemplate.kt
    
    * Update APITemplate.kt
    
    * maybe
    
    * Update APITemplate.kt
    
    * Update APITemplate.kt
    
    * renames
    
    * Update APITemplate.kt
    
    * Update LoyaltyNonce.kt
    
    * added LoyaltyPreAuth request
    
    * Update LoyaltyPreAuth.kt
    
    * comments
    
    * builds

--- sdk/src/main/java/com/karhoo/sdk/api/service/loyalty/KarhooLoyaltyService.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/service/loyalty/KarhooLoyaltyService.kt
@@ -2,8 +2,12 @@ package com.karhoo.sdk.api.service.loyalty
 
 import com.karhoo.sdk.api.datastore.credentials.CredentialsManager
 import com.karhoo.sdk.api.model.LoyaltyBalance
+import com.karhoo.sdk.api.model.LoyaltyPoints
 import com.karhoo.sdk.api.model.LoyaltyConversion
+import com.karhoo.sdk.api.model.LoyaltyNonce
+import com.karhoo.sdk.api.model.LoyaltyStatus
 import com.karhoo.sdk.api.network.client.APITemplate
+import com.karhoo.sdk.api.network.request.LoyaltyPreAuthPayload
 import com.karhoo.sdk.call.Call
 import javax.inject.Inject
 
@@ -22,4 +26,37 @@ class KarhooLoyaltyService : LoyaltyService {
     override fun getConversionRates(loyaltyID: String): Call<LoyaltyConversion> = LoyaltyConversionInteractor(credentialsManager, apiTemplate).apply {
         this.loyaltyId = loyaltyID
     }
+
+    override fun getLoyaltyStatus(loyaltyID: String): Call<LoyaltyStatus> = LoyaltyStatusInteractor(credentialsManager, apiTemplate).apply {
+        this.loyaltyId = loyaltyID
+    }
+
+    override fun getLoyaltyBurn(
+        loyaltyID: String,
+        currency: String,
+        cents: Int): Call<LoyaltyPoints>  = LoyaltyPointsInteractor(credentialsManager, apiTemplate).apply {
+        this.loyaltyId = loyaltyID
+        this.currency = currency
+        this.amount = cents
+        this.toEarn = false
+    }
+
+    override fun getLoyaltyEarn(
+        loyaltyID: String,
+        currency: String,
+        totalAmount: Int,
+        burnPoints: Int
+                               ): Call<LoyaltyPoints> = LoyaltyPointsInteractor(credentialsManager, apiTemplate).apply {
+        this.loyaltyId = loyaltyID
+        this.currency = currency
+        this.totalAmount = totalAmount
+        this.burnPoints = burnPoints
+        this.toEarn = true
+    }
+
+    override fun getLoyaltyPreAuth(loyaltyID: String, request: LoyaltyPreAuthPayload): Call<LoyaltyNonce> =
+        LoyaltyPreAuthInteractor(credentialsManager, apiTemplate).apply {
+            this.loyaltyId = loyaltyID
+            this.preAuthRequest = request
+        }
 }
