bf050bca073650497ff66d7afe7cbafc11fa178f
mateid-kr <matei.dediu@karhoo.com>
2021-12-03 16:25:12 +0200

    MOB-3656 Correct loyalty programme key

--- sdk/src/main/java/com/karhoo/sdk/api/model/Provider.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/model/Provider.kt
@@ -6,6 +6,6 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Provider(@SerializedName("id") val id: String = "",
-                    @SerializedName("loyalty_programmes") val loyalty:
+                    @SerializedName("loyalty_programme") val loyalty:
                            LoyaltyProgramme? = null)
     : Parcelable
