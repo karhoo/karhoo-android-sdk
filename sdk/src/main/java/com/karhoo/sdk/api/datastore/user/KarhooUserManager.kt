6b2aabf8e9e8bbf230438783c27295d9d77c58ff
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-04-06 16:04:43 +0300

    Update KarhooUserManager.kt

--- sdk/src/main/java/com/karhoo/sdk/api/datastore/user/KarhooUserManager.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/datastore/user/KarhooUserManager.kt
@@ -128,7 +128,7 @@ class KarhooUserManager(private val sharedPreferences: SharedPreferences,
 
     private fun returnSavedPaymentProvider(): PaymentProvider? {
         val id = sharedPreferences.getString(PROVIDER_ID, "").orEmpty()
-        val version = sharedPreferences.getString(PROVIDER_VERSION, "")
+        val version = sharedPreferences.getString(PROVIDER_VERSION, null)
         val loyaltyProgrammes = getLoyaltyProgramForUser()
 
         return if (id.isNotBlank()) PaymentProvider(provider = Provider(id, version), loyalty = loyaltyProgrammes) else null
