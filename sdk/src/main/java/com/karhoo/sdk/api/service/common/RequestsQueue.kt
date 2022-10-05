15b380b8c1bdb50a4983a761e488800c172f12c7
mateid-kr <matei.dediu@karhoo.com>
2022-10-05 14:26:14 +0300

    MOB-4125 (3/3) Added logging

--- sdk/src/main/java/com/karhoo/sdk/api/service/common/RequestsQueue.kt
+++ sdk/src/main/java/com/karhoo/sdk/api/service/common/RequestsQueue.kt
@@ -1,21 +1,27 @@
 package com.karhoo.sdk.api.service.common
 
+import com.karhoo.sdk.BuildConfig
 import com.karhoo.sdk.api.network.observable.Observable.Companion.BASE_POLL_TIME
 import com.karhoo.sdk.api.network.response.Resource
 import kotlinx.coroutines.delay
+import android.util.Log
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicBoolean
 
 internal object RequestsQueue {
     private var queue: ConcurrentLinkedQueue<DelayedRequest<Any>> = ConcurrentLinkedQueue()
     private var processing: AtomicBoolean = AtomicBoolean(false)
+    private const val TAG = "RequestsQueue"
 
     class DelayedRequest<RESPONSE> {
         var subscriber: (Resource<RESPONSE>) -> Unit
         var baseInteractor: BaseCallInteractor<RESPONSE>? = null
         var pollInteractor: BasePollCallInteractor<RESPONSE>? = null
 
-        constructor(subscriber: (Resource<RESPONSE>) -> Unit, interactor: BaseCallInteractor<RESPONSE>) {
+        constructor(
+            subscriber: (Resource<RESPONSE>) -> Unit,
+            interactor: BaseCallInteractor<RESPONSE>
+        ) {
             this.subscriber = subscriber
             this.baseInteractor = interactor
         }
@@ -31,15 +37,21 @@ internal object RequestsQueue {
     }
 
     fun addRequest(request: DelayedRequest<Any>) {
+        log("Adding request to queue")
+
         queue.add(request)
     }
 
     suspend fun consumeRequests() {
-        if(!processing.get()) {
+        if (!processing.get()) {
+            log("Starting to consume requests from the queue - queue size " + queue.size)
+
             processing.set(true)
 
-            while(queue.peek() != null) {
+            while (queue.peek() != null) {
                 val delayedRequest = queue.poll()
+
+                log("Consuming request from queue")
 
                 delayedRequest?.baseInteractor?.let {
                     delayedRequest.subscriber(it.createRequest().await())
@@ -50,7 +62,15 @@ internal object RequestsQueue {
                 }
             }
 
+            log("All requests from the queue have been consumed")
+
             processing.set(false)
+        }
+    }
+
+    private fun log(text: String) {
+        if (BuildConfig.DEBUG) {
+            Log.i(TAG, text)
         }
     }
 }
